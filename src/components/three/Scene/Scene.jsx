e15d74c24a532c6e755aedd68be95d7a4b6a57f2
unknown <partharora557@gmail.com>
2023-06-04 01:21:50 +0530

    doone

--- src/components/three/Scene/Scene.jsx
+++ src/components/three/Scene/Scene.jsx
@@ -9,6 +9,7 @@ const Scene = () => {
   const { scene: scene } = useGLTF("/pencil.glb");
   const { scene: scene1 } = useGLTF("/Lightbulb.glb");
   const { scene: scene2 } = useGLTF("/9_Logo.glb");
+  const { scene: scene3 } = useGLTF("/PR_communication.glb");
   const ref1 = useRef();
   const ref2 = useRef();
   const ref3 = useRef();
@@ -17,22 +18,28 @@ const Scene = () => {
     ref1.current.rotation.y += 0.01;
     ref2.current.rotation.y += 0.01;
     ref3.current.rotation.y += 0.01;
+    // ref5.current.rotation.y += 0.01;
   });
 
   const curr = useSelector((state) => state.scroll.curr);
   const [check, setCheck] = useState(false);
   const [scaledown, setScaleDown] = useState(true);
 
-  const { scale1, position1, scale2, position2, scale3, position3 } = useSpring(
-    {
-      position1: scaledown ? [0, -0.45, 0] : [0, -0.1, 0],
-      scale1: scaledown ? [0.05, 0.05, 0.05] : [0.01, 0.01, 0.01],
-      position2: scaledown ? [0, 0.35, 0] : [0, 0.1, 0],
-      scale2: scaledown ? [0.03, 0.03, 0.03] : [0.01, 0.01, 0.01],
-      position3: scaledown ? [0, -0.68, 0] : [0, -0.23, 0],
-      scale3: scaledown ? [0.03, 0.03, 0.03] : [0.01, 0.01, 0.01],
-    }
-  );
+  const {
+    scale1,
+    position1,
+    scale2,
+    position2,
+    scale3,
+    position3,
+  } = useSpring({
+    position1: scaledown ? [0, -0.45, 0] : [0, -0.1, 0],
+    scale1: scaledown ? [0.05, 0.05, 0.05] : [0.01, 0.01, 0.01],
+    position2: scaledown ? [0, 0.35, 0] : [0, 0.1, 0],
+    scale2: scaledown ? [0.03, 0.03, 0.03] : [0.01, 0.01, 0.01],
+    position3: scaledown ? [0, -0.68, 0] : [0, -0.23, 0],
+    scale3: scaledown ? [0.03, 0.03, 0.03] : [0.01, 0.01, 0.01],
+  });
 
   const [realcurr, setRealCurr] = useState(0);
 
@@ -56,7 +63,7 @@ const Scene = () => {
         <animated.group ref={ref1} position={position1} scale={scale1}>
           <primitive object={scene}>
             <meshBasicMaterial transparent opacity={0.5} />
-          </primitive>ś
+          </primitive>
         </animated.group>
       </group>
       <group position={[realcurr === 1 ? 0 : 10, 0, 0]}>
@@ -69,6 +76,12 @@ const Scene = () => {
           <primitive object={scene2} />
         </animated.group>
       </group>
+      {/* 
+      <group position={[realcurr === 4 ? 0 : 10, 0, 0]}>
+        <animated.group ref={ref5} position={position3} scale={scale3}>
+          <primitive object={scene2} />
+        </animated.group>
+      </group> */}
     </>
   );
 };
