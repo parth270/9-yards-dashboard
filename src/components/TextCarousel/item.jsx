8edbd9ecb257e4cb1da214ce20a04efaf4ff498c
unknown <partharora557@gmail.com>
2023-06-03 14:35:39 +0530

    done

--- src/components/TextCarousel/item.jsx
+++ src/components/TextCarousel/item.jsx
@@ -7,18 +7,19 @@ const LitteSpan = ({ title, id, real }) => {
   const ref = useRef();
   const scroll = useSelector((state) => state.scroll.scroll);
   useEffect(() => {
-    console.log("check");
     setTimeout(() => {
       const rect = ref.current.getBoundingClientRect().x;
       const w = window.innerWidth;
-      const margin = (w * 30) / 100;
+      const margin = (w * 25) / 100;
       const lMargin = margin;
       const rMargin = w - margin;
+
       if (rect < lMargin && lMargin > -rect) {
         const prog = (rect + margin) / (margin * 2);
         setProgress((1 - prog) * 360);
-      } else if (rect > rMargin) {
-        console.log("right");
+      } else if (rect > rMargin && rect < w + margin) {
+        const prog = (w - rect) / (margin * 2);
+        setProgress(prog * 360);
       } else {
         setProgress(0);
       }
